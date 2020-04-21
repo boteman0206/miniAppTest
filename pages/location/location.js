ec5cdff3fa14551bedfbf64d7d13ff408ac3bb45
Your Name <pwaip@foxmail.com>
2020-04-21 18:07:16 +0800

    预览图片

--- pages/location/location.js
+++ pages/location/location.js
@@ -17,6 +17,17 @@ Page({
           lat: latitude
         })
       }
-    })
+    }),
+	wx.previewImage({
+	  current: 'https://img1.gtimg.com/10/1048/104857/10485726_980x1200_0.jpg',
+	  urls: [ // 所有图片的URL列表，数组格式
+	    'https://img1.gtimg.com/10/1048/104857/10485731_980x1200_0.jpg',
+	    'https://img1.gtimg.com/10/1048/104857/10485726_980x1200_0.jpg',
+	    'https://img1.gtimg.com/10/1048/104857/10485729_980x1200_0.jpg'
+	  ],
+	  success: function(res) {
+	    console.log(res)
+	  }
+	})
   }
 })
